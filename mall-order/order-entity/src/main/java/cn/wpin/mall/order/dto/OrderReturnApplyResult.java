c60a21736bc7d227c2179631c37c0dd60eee5a4b
wangpin <1151749070@qq.com>
2020-06-25 21:31:01 +0800

    init order-entity project

---
+++ mall-order/order-entity/src/main/java/cn/wpin/mall/order/dto/OrderReturnApplyResult.java
@@ -0,0 +1,17 @@
+package cn.wpin.mall.order.dto;
+
+import cn.wpin.mall.order.entity.CompanyAddress;
+import cn.wpin.mall.order.entity.OrderReturnApply;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 申请信息封装
+ * @author wangpin
+ */
+public class OrderReturnApplyResult extends OrderReturnApply {
+
+    @Getter
+    @Setter
+    private CompanyAddress companyAddress;
+}
