b79e54686e843199490e1dfef947eb4b0bb84f35
wangpin <1151749070@qq.com>
2020-07-08 20:43:47 +0800

    update zookeeper address

--- mall-order/order-server/src/main/java/cn/wpin/mall/order/OrderApplication.java
+++ mall-order/order-server/src/main/java/cn/wpin/mall/order/OrderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
+ * 基于dubbo配置中心
  * @author wangpin
  */
 @SpringBootApplication
